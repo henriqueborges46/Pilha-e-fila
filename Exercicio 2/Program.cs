4b2b3fa434b634c6ec2a12deb3992aa47aa7a061
henriqueborges46 <hiquehumberto007@gmail.com>
2025-04-15 19:51:19 -0300

    Add files via upload

---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Queue<string> fila = new Queue<string>();
+
+
+        fila.Enqueue("Ana");
+        fila.Enqueue("Carlos");
+        fila.Enqueue("Beatriz");
+        fila.Enqueue("Daniel");
+
+
+        fila.Dequeue();
+
+
+        Console.WriteLine("Elementos restantes na fila:");
+        foreach (string nome in fila)
+        {
+            Console.WriteLine(nome);
+        }
+    }
+}
